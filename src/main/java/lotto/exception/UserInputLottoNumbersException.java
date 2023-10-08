4b5a741557f46770beb79f18b291cb29546d0fa6
민주 <hinote444@gmail.com>
2023-10-08 22:12:51 +0900

    feat: 로또 번호가 유효한 범위인지 검사하는 예외 구현

--- src/main/java/lotto/exception/UserInputLottoNumbersException.java
+++ src/main/java/lotto/exception/UserInputLottoNumbersException.java
@@ -1,15 +1,27 @@
 package lotto.exception;
 
+import java.util.List;
+
 public class UserInputLottoNumbersException {
     private final static int VALID_LOTTO_LENGTH = 6;
     private final static int VALID_BONUS_LENGTH = 1;
+    private final static int MIN_LOTTO_NUMBER = 1;
+    private final static int MAX_LOTTO_NUMBER = 45;
 
     public void validateLotto(String numbers, String bonusNumber) {
         validateLottoNumbersLength(numbers);
     }
 
     public void validateLottoNumbersLength(String inputNumbers) {
-        if (inputNumbers.replace(",", "").length() != VALID_LOTTO_LENGTH)
+        if (inputNumbers.replace(",", "").length() != VALID_LOTTO_LENGTH) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateNumbersRange(List<Integer> inputNumbers) {
+        for (int input : inputNumbers) {
+            if (input < MIN_LOTTO_NUMBER || input > MAX_LOTTO_NUMBER)
+                throw new IllegalArgumentException();
+        }
     }
 }
