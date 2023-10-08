2ea32f23a5c75c6563e167e596e2b2b6ad0cfb68
민주 <hinote444@gmail.com>
2023-10-09 03:18:19 +0900

    refactor(LottoGenerator): 로또 생성 로직 변경

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -1,12 +1,17 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.UserLotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    public static List<Integer> generatorLottoNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
+    public static List<UserLotto> generatorUserLotto(int quantity) {
+        List<UserLotto> userLotto = new ArrayList<>();
+        for(int i=0; i<quantity; i++){
+            userLotto.add(new UserLotto(Randoms.pickUniqueNumbersInRange(1,45,6)));
+        }
+        return userLotto;
     }
 }
