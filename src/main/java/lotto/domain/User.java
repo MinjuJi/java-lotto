2336b01acafd1c3acc971d3d6e4b279b3588855b
민주 <hinote444@gmail.com>
2023-10-09 03:25:18 +0900

    feat(User): 구입 금액과 구매한 로또를 관리해주는 도메인 생성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,12 +1,19 @@
 package lotto.domain;
 
+import lotto.util.LottoGenerator;
+
 import java.util.List;
 
 public class User {
-    private int buyingPrice;
-    private List<UserLotto> lottos;
+    private final int buyingPrice;
+    private final List<UserLotto> lotto;
 
     public User(int buyingPrice) {
         this.buyingPrice = buyingPrice;
+        this.lotto = LottoGenerator.generatorUserLotto(getLottoQuantity());
+    }
+
+    private int getLottoQuantity() {
+        return this.buyingPrice / 1000;
     }
 }
