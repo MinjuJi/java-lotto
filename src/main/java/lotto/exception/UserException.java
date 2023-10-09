2e4e18f9bfb479079b868c8f3b39240282f75c5a
민주 <hinote444@gmail.com>
2023-10-09 12:44:02 +0900

    feat(UserException): 유저 구입 금액 입력 예외 기능 구현

---
+++ src/main/java/lotto/exception/UserException.java
@@ -0,0 +1,30 @@
+package lotto.exception;
+
+public class UserException {
+
+    private void checkContainsOnlyDigits(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                exception("구입 금액은 숫자로 이루어져야 합니다.");
+            }
+        }
+    }
+
+    private void checkPriceIsNotZero(String input) {
+        int price = Integer.parseInt(input);
+        if (price == 0) {
+            exception("구입 최소 금액은 1000원입니다.");
+        }
+    }
+
+    private void checkPrice(String input) {
+        int price = Integer.parseInt(input);
+        if (price % 1000 != 0) {
+            exception("구입 금액은 1000원 단위입니다.");
+        }
+    }
+
+    private void exception(String message) {
+        throw new IllegalArgumentException(message);
+    }
+}
