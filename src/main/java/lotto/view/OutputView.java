a4c8b32cd1f962a615dd10b7945abcfd65c60266
민주 <hinote444@gmail.com>
2023-10-09 10:57:02 +0900

    refactor(OutputView): 출력 형식을 맞추기 위해 줄바꿈 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,25 +9,34 @@ public class OutputView {
         System.out.println("구입 금액을 입력해주세요.");
     }
 
-    public void printBuyingQuantityOfLotto(int quantity) {
-        System.out.println(quantity + "개를 구매했습니다.");
+    public void printBuyingQuantityOfLotto(int buyingPrice) {
+        System.out.println("\n" + getBuyingQuantity(buyingPrice) + "개를 구매했습니다.");
+    }
+
+    private int getBuyingQuantity(int buyingPrice) {
+        return buyingPrice / 1000;
     }
 
     public void printUserLottos(List<UserLotto> userLottos) {
 //      userLottos.stream().forEach(System.out::println); 아래와 같은지 확인
-        userLottos.stream().forEach(lotto -> printLotto(lotto.getLotto()));
+        userLottos.stream()
+                  .forEach(lotto -> printLotto(lotto.getLotto()));
     }
 
     public void printLotto(List<Integer> lotto) {
         System.out.println(lotto);
     }
 
-    public void printAskCorrectNumbers(){
-        System.out.println("당첨 번호를 입력해주세요.");
+    public void printAskCorrectNumbers() {
+        System.out.println("\n당첨 번호를 입력해주세요.");
     }
 
-    public void printResult(){
-        System.out.println("당첨 통계");
+    public void printAskBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+
+    public void printResult() {
+        System.out.println("\n당첨 통계");
         System.out.println("---");
     }
 }
