05841a8db79937f04325bad9da3ffc0f6507e652
민주 <hinote444@gmail.com>
2023-10-09 10:59:21 +0900

    feat(Lotto): 보너스 번호를 입력받기 위한 필드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,11 +3,14 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public Lotto(List<Integer> numbers, int bonusNumber) {
         validate(numbers);
         this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
     }
 
     private void validate(List<Integer> numbers) {
