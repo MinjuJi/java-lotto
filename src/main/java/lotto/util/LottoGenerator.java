0267a9c65597b1ae41209f083d8e85c6ce76eaec
민주 <hinote444@gmail.com>
2023-10-09 10:49:10 +0900

    refactor(LottoGenerator): 로또 넘버가 정렬 후 반환되도록 리팩토링

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -4,14 +4,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.UserLotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
     public static List<UserLotto> generatorUserLotto(int quantity) {
         List<UserLotto> userLotto = new ArrayList<>();
-        for(int i=0; i<quantity; i++){
-            userLotto.add(new UserLotto(Randoms.pickUniqueNumbersInRange(1,45,6)));
+        for (int i = 0; i < quantity; i++) {
+            userLotto.add(new UserLotto(createLotto()));
         }
         return userLotto;
+    }
+
+    private static List<Integer> createLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
     }
 }
