f38e7832b1be777aa09cecd05ea004140f3aa8bc
민주 <hinote444@gmail.com>
2023-10-10 11:14:16 +0900

    feat(validation): 하나의 숫자가 특정 범위 안의 값인지 검증하는 기능

--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -1,4 +1,9 @@
 package lotto.validation;
 
 public class Validation {
+    public static void validateNumberInRange(int number, int startInclusive, int endInclusive) {
+        if (number < startInclusive || number > endInclusive) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
