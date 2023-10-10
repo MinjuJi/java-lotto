c5c53dc90ad0eb3ff8605f24398eff1828b7f81e
민주 <hinote444@gmail.com>
2023-10-10 23:20:21 +0900

    feat(controller): 로또게임 시작 기능

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,17 @@
 package lotto.controller;
 
+import lotto.service.Service;
+
 public class Controller {
+    Service service = new Service(); // 접근제어자, static, final 붙이지 않는 이유
+
+    public void run() {
+        startLottoGame();
+    }
+
+    public void startLottoGame() {
+        service.initBuyer();
+        service.buyAllLotto();
+        service.showBuyingResult();
+    }
 }
