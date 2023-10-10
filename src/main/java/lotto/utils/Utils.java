9ab6442fa0731923591cc43609f1df909ec9b788
민주 <hinote444@gmail.com>
2023-10-10 12:52:56 +0900

    feat(utils): 지정 범위 내에서 원하는 개수의 수를 무작위 생성하는 기능

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -1,9 +1,15 @@
 package lotto.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Utils {
+    public static List<Integer> GenerateRandomUniqueNumber(int start, int end, int count) {
+        return Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+
     public static void sortListNaturalOrder(List<Integer> list) {
         Collections.sort(list);
     }
