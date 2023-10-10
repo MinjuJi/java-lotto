185e974a558f73fb3f49888ca858ca64f5ac5750
민주 <hinote444@gmail.com>
2023-10-11 06:44:37 +0900

    feat(utils): 두 리스트를 비교하여 일치하는 요소들의 개수를 확인하는 기능
    주어진 두 수가 같은 수인지 확인해 같으면 count하는 countSameNumber
    메소드 구현
    
    주어진 두 리스트의 각 요소들을 비교하여 동일한 위치에 같은 값인 요소들이
    몇 개인지 count 하는 countSameElements 메소드 구현

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -8,6 +8,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
+    private static final int SAME = 1;
+    private static final int NOT_SAME = 0;
+
     public static List<Integer> generateRandomUniqueNumber(int start, int end, int count) {
         return Randoms.pickUniqueNumbersInRange(start, end, count);
     }
@@ -29,11 +32,25 @@ public class Utils {
     }
 
 // stringToIntegerList() 와 stringToIntArray()를 합친 메소드. 이게 더 간결하지 않나?
-//    public static List<Integer> stringToIntegerList2(String string) {
+//    public static List<Integer> stringToIntegerList(String string) {
 //        return Arrays.stream(string.split(","))
 //                     .mapToInt(Integer::parseInt)
 //                     .boxed()
 //                     .collect(Collectors.toList());
 //    }
 
+    public static int countSameElements(List<Integer> list1, List<Integer> list2) {
+        int count = 0;
+        for (int index = 0; index < list1.size(); index++) {
+            count += countSameNumber(list1.get(index), list2.get(index));
+        }
+        return count;
+    }
+
+    public static int countSameNumber(int number1, int number2) {
+        if (number1 == number2) {
+            return SAME;
+        }
+        return NOT_SAME;
+    }
 }
