1f9bade9cba0fc65e844958ecb502f6d505a466a
민주 <hinote444@gmail.com>
2023-10-10 12:35:04 +0900

    feat(utils): 숫자 리스트를 오름차순으로 정렬하는 기능

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -1,4 +1,10 @@
 package lotto.utils;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Utils {
+    public static void sortListNaturalOrder(List<Integer> list) {
+        Collections.sort(list);
+    }
 }
