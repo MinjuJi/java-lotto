d18711693c5b4be3631fb66903a3b47ae03f2446
민주 <hinote444@gmail.com>
2023-10-10 12:08:29 +0900

    feat(validation): 숫자 리스트 내에 중복 값이 있는지 검증하는 기능

--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -1,8 +1,18 @@
 package lotto.validation;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Validation {
     public static void validateNumberInRange(int number, int startInclusive, int endInclusive) {
         if (number < startInclusive || number > endInclusive) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateDuplicationList(List<Integer> list) {
+        HashSet<Integer> set = new HashSet<>(list);
+        if (list.size() != set.size()) {
             throw new IllegalArgumentException();
         }
     }
