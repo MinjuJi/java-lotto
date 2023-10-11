0536b293c68a8e55d847872c48ac841b932b871b
민주 <hinote444@gmail.com>
2023-10-11 10:34:17 +0900

    fix(utils): 유틸 메소드 수정 수정사항
    두 리스트에서 위치와 값 모두 일치하는 요소값의 개수를 세는 것에서
    두 리스트에서 값이 일치하는 요소값의 개수를 세는 것으로 변경
    
    수정사유
    로또 당첨 기준 요구사항 착오로 인한 수정

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
-    private static final int SAME = 1;
-    private static final int NOT_SAME = 0;
+    private static final int CONTAIN = 1;
+    private static final int NOT_CONTAIN = 0;
 
     public static List<Integer> generateRandomUniqueNumber(int start, int end, int count) {
         return Randoms.pickUniqueNumbersInRange(start, end, count);
@@ -41,16 +41,16 @@ public class Utils {
 
     public static int countSameElements(List<Integer> list1, List<Integer> list2) {
         int count = 0;
-        for (int index = 0; index < list1.size(); index++) {
-            count += countSameNumber(list1.get(index), list2.get(index));
+        for (int number : list2) {
+            count += countContainsNumber(list1, number);
         }
         return count;
     }
 
-    public static int countSameNumber(int number1, int number2) {
-        if (number1 == number2) {
-            return SAME;
+    public static int countContainsNumber(List<Integer> list, int number) {
+        if (list.contains(number)) {
+            return CONTAIN;
         }
-        return NOT_SAME;
+        return NOT_CONTAIN;
     }
 }
