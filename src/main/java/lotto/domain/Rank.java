af272fe982444df9cd22501064586f390c971923
민주 <hinote444@gmail.com>
2023-10-11 11:04:07 +0900

    fix(domain): enum 객체 수정
    수정사항
    - enum 값들의 순서 변경
    - 당첨금액 문자열 표현 값 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,21 +1,23 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NO_RANK(0, false, 0); // 1, 2는??
+    NO_RANK(0, false, 0, "0"), // 1, 2는??
+    FIFTH(3, false, 5_000, "5_000"),
+    FOURTH(4, false, 50_000, "50_000"),
+    THIRD(5, false, 1_500_000, "1_500_000"),
+    SECOND(5, true, 30_000_000, "30_000_000"),
+    FIRST(6, false, 2_000_000_000, "2_000_000_000");
 
     final private int count;
     final private boolean isBonus;
     final private long prize;
+    final private String convertPrize;
 
-    Rank(int count, boolean isBonus, long prize) {
+    Rank(int count, boolean isBonus, long prize, String convertPrize) {
         this.count = count;
         this.isBonus = isBonus;
         this.prize = prize;
+        this.convertPrize = convertPrize;
     }
 
     public int getCount() {
@@ -28,5 +30,9 @@ public enum Rank {
 
     public long getPrize() {
         return prize;
+    }
+
+    public String getConvertPrize() {
+        return convertPrize;
     }
 }
