0ab76064ada308ba9c10d8b8f70f5ae4e5d7868c
민주 <hinote444@gmail.com>
2023-10-11 13:59:20 +0900

    fix(domain): long 타입 변수 할당값에 L 표기

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,12 +1,12 @@
 package lotto.domain;
 
 public enum Rank {
-    NO_RANK(0, false, 0, "0"), // isBonus가 필요한가?
-    FIFTH(3, false, 5_000, "5_000"),
-    FOURTH(4, false, 50_000, "50_000"),
-    THIRD(5, false, 1_500_000, "1_500_000"),
-    SECOND(5, true, 30_000_000, "30_000_000"),
-    FIRST(6, false, 2_000_000_000, "2_000_000_000");
+    NO_RANK(0, false, 0L, "0"), // isBonus가 필요한가?
+    FIFTH(3, false, 5_000L, "5_000"),
+    FOURTH(4, false, 50_000L, "50_000"),
+    THIRD(5, false, 1_500_000L, "1_500_000"),
+    SECOND(5, true, 30_000_000L, "30_000_000"),
+    FIRST(6, false, 2_000_000_000L, "2_000_000_000");
 
     final private int count;
     final private boolean isBonus;
