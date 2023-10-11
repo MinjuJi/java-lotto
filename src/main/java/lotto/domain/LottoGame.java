348ddbb14d8fe3006d874b15d01c127122552ef3
민주 <hinote444@gmail.com>
2023-10-11 16:52:00 +0900

    refactor(service): service 단에서 view 관련 기능 분리
    view 관련 기능은 모두 controller 단으로 이관.
    service 단에는 도메인 로직만 남기도록 리팩토링

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -8,12 +8,14 @@ public class LottoGame {
     private static final int LOTTO_LENGTH = 6;
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_LAST_NUMBER = 45;
-    private List<Integer> winningNumbers;
-    private int bonusNumber;
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
 
-    public LottoGame(List<Integer> winningNumbers) {
+    public LottoGame(List<Integer> winningNumbers, int bonusNumber) {
         validateWinningNumbers(winningNumbers);
         this.winningNumbers = winningNumbers;
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
     }
 
     public List<Integer> getWinningNumbers() {
@@ -22,11 +24,6 @@ public class LottoGame {
 
     public int getBonusNumber() {
         return bonusNumber;
-    }
-
-    public void setBonusNumber(int bonusNumber) { // 생성자에 함께 작성하면 되지 않나? 그에 따라 Servic 클래스의 playLottoGame 메소드 수정 필요
-        validateBonusNumber(bonusNumber);
-        this.bonusNumber = bonusNumber;
     }
 
     public void validateWinningNumbers(List<Integer> winningNumbers) {
--- src/main/java/lotto/service/Service.java
+++ src/main/java/lotto/service/Service.java
@@ -5,111 +5,63 @@ import lotto.domain.Lotto;
 import lotto.domain.LottoGame;
 import lotto.domain.Rank;
 import lotto.utils.Utils;
-import lotto.view.InputMessage;
-import lotto.view.OutputMessage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
-
 public class Service {
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_LAST_NUMBER = 45;
     private static final int LOTTO_LENGTH = 6;
-    private static final int LOTTO_LEAST_AMOUNT = 1_000; // 변수명 수정 필요
+    private static final int LOTTO_LEAST_AMOUNT = 1_000;
     private static final int COUNT_ZERO = 0;
     private static final int COUNT_ONE = 1;
 
-    Buyer buyer; // 접근제어자 안 붙이는 이유
-    LottoGame lottoGame;
-
-    public void initBuyer() {
-        buyer = new Buyer(getInputAmount());
-    }
-
-    public int getInputAmount() {
-        InputMessage.inputAmount();
-        String userInput = readLine().trim(); // trim() 하는 이유
-        return Utils.stringToInteger(userInput);
-    }
-
-    public void buyAllLotto() {
-        int count = (buyer.getPurchaseAmount() / LOTTO_LEAST_AMOUNT); // 메소드로 분리
-        for (int i = count; i > 0; i--) {
-            buyOneLotto();
+    public void buyAllLotto(Buyer buyer) {
+        int count = buyer.getPurchaseAmount() / LOTTO_LEAST_AMOUNT;
+        for (; count > COUNT_ZERO; count--) {
+            buyOneLotto(buyer);
         }
     }
 
-    public void buyOneLotto() {
+    private void buyOneLotto(Buyer buyer) {
         Lotto lotto = generateLottoNumber();
         buyer.buyLotto(lotto);
     }
 
-    public void showBuyingResult() {
-        OutputMessage.purchaseComplete(buyer.getPurchaseAmount() / LOTTO_LEAST_AMOUNT); // 메소드로 분리
-        for (Lotto lotto : buyer.getPurchasedLotteries()) {
-            OutputMessage.lottoNumbers(lotto.getNumbers());
-        }
-    }
-
-    public Lotto generateLottoNumber() {
-        List<Integer> numbers = new ArrayList<>(Utils.generateRandomUniqueNumber(LOTTO_START_NUMBER, LOTTO_LAST_NUMBER, LOTTO_LENGTH));
+    private Lotto generateLottoNumber() {
+        List<Integer> numbers = new ArrayList<>(
+                Utils.generateRandomUniqueNumber(LOTTO_START_NUMBER, LOTTO_LAST_NUMBER, LOTTO_LENGTH));
         Utils.sortListNaturalOrder(numbers);
         return new Lotto(numbers);
     }
 
-    public List<Integer> getInputWinningNumbers() {
-        InputMessage.inputWinningNumbers();
-        return Utils.stringToIntegerList(readLine().trim());
-    }
-
-    public int getInputBonusNumber() {
-        InputMessage.inputBonusNumber();
-        return Utils.stringToInteger(readLine().trim());
-    }
-
-    public void playLottoGame() {
-        lottoGame = new LottoGame(getInputWinningNumbers());
-        lottoGame.setBonusNumber(getInputBonusNumber());
-    }
-
-    public int countCorrectLottoNumbers(Lotto lotto) {
-        return Utils.countSameElements(lottoGame.getWinningNumbers(), lotto.getNumbers());
-    }
-
-    public boolean containsBonusNumber(Lotto lotto) {
-        return lotto.getNumbers()
-                    .contains(lottoGame.getBonusNumber());
-    }
-
-    public void checkBuyerLotteries() { // result가 메소드 내에서만 존재하지 않나?
+    public void checkBuyerLotteries(Buyer buyer, LottoGame lottoGame) {
         HashMap<Rank, Integer> result = buyer.getLottoResult();
-
         for (Lotto lotto : buyer.getPurchasedLotteries()) {
-            Rank rank = determineLottoRank(lotto);
+            Rank rank = determineLottoRank(lottoGame, lotto);
             result.put(rank, result.getOrDefault(rank, COUNT_ZERO) + COUNT_ONE);
         }
     }
 
-    public Rank determineLottoRank(Lotto lotto) {
-        int correctCount = countCorrectLottoNumbers(lotto);
-        boolean correctBonusNumber = containsBonusNumber(lotto);
+    private int countCorrectLottoNumbers(LottoGame lottoGame, Lotto lotto) {
+        return Utils.countSameElements(lottoGame.getWinningNumbers(), lotto.getNumbers());
+    }
 
+    private boolean containsBonusNumber(LottoGame lottoGame, Lotto lotto) {
+        return lotto.getNumbers()
+                    .contains(lottoGame.getBonusNumber());
+    }
+
+    private Rank determineLottoRank(LottoGame lottoGame, Lotto lotto) {
+        int correctCount = countCorrectLottoNumbers(lottoGame, lotto);
+        boolean correctBonusNumber = containsBonusNumber(lottoGame, lotto);
         for (Rank rank : Rank.values()) {
             if (correctCount == rank.getCount() && correctBonusNumber == rank.getIsBonus()) {
                 return rank;
             }
         }
         return Rank.NO_RANK;
-    }
-
-
-    public void showGameResult() {
-        OutputMessage.winningStatistics();
-        OutputMessage.showRankResult(buyer.getLottoResult());
-        OutputMessage.showYieldResult(buyer.getYield());
     }
 }
